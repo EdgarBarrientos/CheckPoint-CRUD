687c694cc9e8c611c0c611050f16b4ac3f1bec1d
EdgarBarrientos <Edgar_O_Barrientos@outlook.com>
2021-06-04 19:06:36 -0400

    initial commit

---
+++ src/main/java/com/cognizant/CheckPoint/CheckPointApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.CheckPoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckPointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckPointApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO User (id,email) VALUES (1,'edgar@email.com');
+INSERT INTO User (id,email) VALUES (2,'edgar2@email.com');
+
